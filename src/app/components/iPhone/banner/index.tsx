018d0850c41fb9a10c13a9a3709f2aabb174069b
CarsonDev1 <buitritinht@gmail.com>
2024-10-05 14:46:18 +0700

    upload code

--- src/app/components/iPhone/banner/index.tsx
+++ src/app/components/iPhone/banner/index.tsx
@@ -11,18 +11,8 @@ const Banner: React.FC = () => {
 	return (
 		<div className='banner'>
 			<Image src='/apple/banner-apple.jpg' alt='banner' width={1820} height={1400} />
-			<div className='container'>
-				{/* <div className='banner-content'>
-					<div className='banner-content-item'>
-						<Image src='/apple/banner-item-01.webp' alt='banner-item-01' width={500} height={400} />
-					</div>
-					<div className='banner-content-item'>
-						<Image src='/apple/banner-item-02.webp' alt='banner-item-02' width={500} height={400} />
-					</div>
-					<div className='banner-content-item'>
-						<Image src='/apple/banner-item-03.webp' alt='banner-item-03' width={500} height={400} />
-					</div>
-				</div> */}
+			<div className='banner-content'>
+				<Image src='/apple/heart.png' alt='heart' width={1200} height={900} />
 			</div>
 		</div>
 	);

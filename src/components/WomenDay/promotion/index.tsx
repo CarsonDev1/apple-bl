21d35da166c9d82d4075857d7c75572001bea62e
CarsonDev1 <buitritinht@gmail.com>
2024-10-17 16:22:58 +0700

    upload code

--- src/components/WomenDay/promotion/index.tsx
+++ src/components/WomenDay/promotion/index.tsx
@@ -26,7 +26,7 @@ const Promotion = ({ onScrollToRules }: PromotionProps) => {
 			<div className='container'>
 				<div className='promotion-header'>5 đặc quyền mua hàng tại Bạch Long Mobile</div>
 				<div className='promotion-list-privilege'>
-					<div onClick={onScrollToRules}>
+					<div onClick={onScrollToRules} style={{ cursor: 'pointer' }}>
 						<Image src={Privilege01} alt='privilege-01' width={270} height={117} />
 					</div>
 					<Link href='https://bachlongmobile.com/thu-cu-doi-moi/'>
@@ -38,7 +38,7 @@ const Promotion = ({ onScrollToRules }: PromotionProps) => {
 					<Link href='https://bachlongmobile.com/combo-phu-kien/'>
 						<Image src={Privilege04} alt='privilege-04' width={270} height={117} />
 					</Link>
-					<div onClick={onScrollToRules}>
+					<div onClick={onScrollToRules} style={{ cursor: 'pointer' }}>
 						<Image src={Privilege05} alt='privilege-05' width={270} height={117} />
 					</div>
 				</div>
